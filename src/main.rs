4902cddb2ca346339c534852b47fc57a5dee9ae4
k1r1m1 <kirimi@paygoenergy.co>
2021-04-26 22:42:08 +0300

    :tada: chaining function calls with ?. operator

--- src/main.rs
+++ src/main.rs
@@ -9,19 +9,9 @@ use std::io;
 use std::io::Read;
 
 fn read_username_from_file_1() -> Result<String, io::Error> {
-    let f = File::open("hello.txt");
-    let mut f = match f {
-        Ok(file) => file,
-        Err(e) => return Err(e), // here we return early from the fn & pass the error value 
-        // from File::open back to the calling code as this fn's error value.
-    };
-
     let mut s = String::new();
-
-    match f.read_to_string(&mut s) {
-        Ok(_) => Ok(s),
-        Err(e) => Err(e), // here, no need to explicitly return, since this is the last expression.
-    }
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+    Ok(s)
 }
 
 fn main() {
